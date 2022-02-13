d2e06cfa1d17d860335941ff8505443d919b8f0f
Seth Lewis <sethappleacount@gmail.com>
2022-02-12 23:12:38 -0800

    Display the 10 most recent messages in global

--- global.php
+++ global.php
@@ -13,7 +13,28 @@ session_start();
 	
 </head>
 <body>
-	This is the global page
+	<!-- display the 10 most recent messages posted to the site -->
+	<?php
+		$host = "localhost";
+		$username = "root";
+		$password = "";
+		$database = "robonetusers";
+		if (!$messages = mysqli_connect($host, $username, $password, $database)){
+			die("failed to connect!");
+		}
+		//if there are any values in the table, display them one at a time
+		$sql = "SELECT messageID, userID, message FROM messages";
+		$result = $messages->query($sql);
+		
+		if ($result->num_rows > 0){
+			// output data of each row
+			while ($row = $result->fetch_assoc()) {
+				echo "Message number: " . $row["messageID"]. " From user: " . $row["userID"]. " " . $row["message"]. "<br>";
+			}
+		} else {
+			echo "0 results";
+		}
+	?>
 
 	<br><br>
 
