32edc53d41814923baccb798cdec73025c8dddd1
Seth Lewis <sethappleacount@gmail.com>
2022-02-13 00:58:06 -0800

    Can add a new message with your userID. Can view 10 items from your personal history and the entire database.

--- global.php
+++ global.php
@@ -5,6 +5,15 @@ session_start();
 	include("checklogin.php");
 
 	$user_data = check_login($con);
+
+	if($_SERVER['REQUEST_METHOD'] == "POST"){
+		$insert_message = $_POST['new_message'];
+		
+		$query = $con->prepare("INSERT INTO messages (messageID, userID, message) VALUES (NULL, ?, ?)");
+		$query->bind_param("is", $user_data['userID'], $insert_message);
+
+		$query->execute();
+	}
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +23,8 @@ session_start();
 </head>
 <body>
 	<!-- display the 10 most recent messages posted to the site -->
+	Here's what people are saying now:
+	<br>
 	<?php
 		$host = "localhost";
 		$username = "root";
@@ -23,7 +34,7 @@ session_start();
 			die("failed to connect!");
 		}
 		//if there are any values in the table, display them one at a time
-		$sql = "SELECT messageID, userID, message FROM messages";
+		$sql = "SELECT messageID, userID, message FROM messages LIMIT 10";
 		$result = $messages->query($sql);
 		
 		if ($result->num_rows > 0){
@@ -35,6 +46,40 @@ session_start();
 			echo "0 results";
 		}
 	?>
+
+	<br><br>
+
+	Your most recent posts:
+	<?php
+		$host = "localhost";
+		$username = "root";
+		$password = "";
+		$database = "robonetusers";
+		if (!$messages = mysqli_connect($host, $username, $password, $database)){
+			die("failed to connect!");
+		}
+		//if there are any values in the table, display them one at a time
+		$sql = "SELECT messageID, userID, message FROM messages WHERE userID = " . strval($user_data['userID'] . " LIMIT 10");
+		$result = $messages->query($sql);
+		
+		if ($result->num_rows > 0){
+			// output data of each row
+			while ($row = $result->fetch_assoc()) {
+				echo "Message number: " . $row["messageID"]. " From user: " . $row["userID"]. " " . $row["message"]. "<br>";
+			}
+		} else {
+			echo "0 results";
+		}
+	?>
+
+	<br><br><br><br>
+
+	<form method="post">
+		<div class="login">Make a post</div><br>
+
+		<input type="text" name="new_message"><br><br>
+		<input class="LoginButton" type="submit" value="submit"><br><br><br>
+	</form>
 
 	<br><br>
 
