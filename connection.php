751269bd22532fa870575e5dd41a4952c95a118f
Seth Lewis <sethappleacount@gmail.com>
2022-02-27 23:16:06 -0800

    Will now create the database and tables automatically if they do not exist.

--- connection.php
+++ connection.php
@@ -5,9 +5,41 @@ $username = "root";
 $password = "";
 $database = "robonetusers";
 
-if (!$con = mysqli_connect($host, $username, $password, $database)){
-	die("failed to connect!");
-}
+$con = mysqli_connect($host, $username, $password);
+$databaseCheck = "CREATE DATABASE IF NOT EXISTS robonetusers";
+mysqli_query($con, $databaseCheck);
+$connectQuery = "use robonetusers";
+mysqli_query($con, $connectQuery);
+$tableCheck = "CREATE TABLE IF NOT EXISTS advertisements(
+	adID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	gender INT(11) UNSIGNED NOT NULL,
+	lowerAge INT(11) UNSIGNED NOT NULL,
+	upperAge INT(11) UNSIGNED NOT NULL,	
+	location varchar(50) NOT NULL,
+	adName varchar(500) NOT NULL,
+	adShowings int(11) UNSIGNED NOT NULL
+)";
+mysqli_query($con, $tableCheck);
+$tableCheck1 = "CREATE TABLE IF NOT EXISTS logininfo(
+	userID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	username varchar(50) NOT NULL,
+	password varchar(50) NOT NULL
+)";
+mysqli_query($con, $tableCheck1);
+$tableCheck2 = "CREATE TABLE IF NOT EXISTS messages(
+	messageID INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	userID INT(11) UNSIGNED NOT NULL,
+	message varchar(500) NOT NULL
+)";
+mysqli_query($con, $tableCheck2);
+$tableCheck3 = "CREATE TABLE IF NOT EXISTS userprofiles(
+	userID INT(11) UNSIGNED NOT NULL,
+	age INT(11) UNSIGNED NOT NULL,
+	gender INT(11) UNSIGNED NOT NULL,
+	location varchar(50) NOT NULL,
+	bio varchar(500) NOT NULL
+)";
+mysqli_query($con, $tableCheck3);
 
 if (!$messages = mysqli_connect($host, $username, $password, $database)){
 	die("failed to connect!");
